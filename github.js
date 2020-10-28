87017e98b92d15d6b6523ab6332d2dcadc700859
delayedaa <delayedaa@gmail.com>
2020-10-28 10:36:10 -0500

    [github.js] Add new object and add new element to page

--- github.js
+++ github.js
@@ -1,7 +1,20 @@
+let container = document.querySelector('.container');
+
 fetch('https://api.github.com/users/delayedaa')
 .then(response => {
   return response.json();
 })
-.then(responseJson => {
-  console.log(responseJson);
+.then(data => {
+  let alanObj = {
+    bio: data.bio,
+    username: data.login,
+    name: data.name
+  }
+  console.log(alanObj);
+  let p = document.createElement('p');
+  p.textContent = alanObj.name;
+  container.appendChild(p);
+})
+.catch(error => {
+  console.log(error);
 });
