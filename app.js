716b6c6f53e8574fe1aeddd3c451d219fce5d22a
delayedaa <delayedaa@gmail.com>
2020-10-28 13:26:30 -0500

    [style.css] Add additional styles

--- app.js
+++ app.js
@@ -8,73 +8,73 @@ let createNewElement = (tagName, className) => {
 
 let createNewCard = (pokemon) => {
   fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    
-    console.log(data);
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
 
-    // Create new object
-    let newPokemon = {
-      imageUrl: data.sprites.other['official-artwork']['front_default'],
-      name: data.name,
-      height: data.height,
-      weight: data.weight,
-      ability: data.abilities[0].ability.name,
-      move: data.moves[0].move.name,
-      purchaseLink: 'https://www.pokemoncenter.com/category/trading-card-game'
-    }
-    
-    // Create new HTML elements
-    let divCard = createNewElement('div', 'card');
-    divCard.classList.add(pokemon);
-    divCard.style.width = '20rem';
-    divCard.style.display = 'inline-block';
+      console.log(data);
 
-    let img = createNewElement('img', 'card-img-top');
-    img.src = newPokemon.imageUrl;
+      // Create new object
+      let newPokemon = {
+        imageUrl: data.sprites.other['official-artwork']['front_default'],
+        name: data.name,
+        height: data.height,
+        weight: data.weight,
+        ability: data.abilities[0].ability.name,
+        move: data.moves[0].move.name,
+        purchaseLink: 'https://www.pokemoncenter.com/category/trading-card-game'
+      }
 
-    let divCardBody = createNewElement('div', 'card-body');
+      // Create new HTML elements
+      let divCard = createNewElement('div', 'card');
+      divCard.classList.add(pokemon);
+      divCard.style.width = '20rem';
+      divCard.style.display = 'inline-block';
 
-    let name = createNewElement('h5', 'card-title');
-    name.textContent = newPokemon.name;
-    name.style.textTransform = 'capitalize';
+      let img = createNewElement('img', 'card-img-top');
+      img.src = newPokemon.imageUrl;
 
-    let height = createNewElement('p', 'card-text');
-    height.textContent = `Height: ${newPokemon.height}`;
+      let divCardBody = createNewElement('div', 'card-body');
 
-    let weight = createNewElement('p', 'card-text');
-    weight.textContent = `Weight: ${newPokemon.weight}`;
+      let name = createNewElement('h5', 'card-title');
+      name.textContent = newPokemon.name;
+      name.style.textTransform = 'capitalize';
 
-    let ability = createNewElement('p', 'card-text');
-    ability.textContent = `Ability: ${newPokemon.ability}`;
+      let height = createNewElement('p', 'card-text');
+      height.textContent = `Height: ${newPokemon.height}`;
 
-    let move = createNewElement('p', 'card-text');
-    move.textContent = `Move: ${newPokemon.move}`;
+      let weight = createNewElement('p', 'card-text');
+      weight.textContent = `Weight: ${newPokemon.weight}`;
 
-    let purchaseLink = createNewElement('a', 'btn');
-    purchaseLink.classList.add('btn-primary');
-    purchaseLink.href = newPokemon.purchaseLink;
-    purchaseLink.textContent = 'Purchase';
+      let ability = createNewElement('p', 'card-text');
+      ability.textContent = `Ability: ${newPokemon.ability}`;
 
-    // Add new HTML elements to the DOM
-    divCardBody.appendChild(name);
-    divCardBody.appendChild(height);
-    divCardBody.appendChild(weight);
-    divCardBody.appendChild(ability);
-    divCardBody.appendChild(move);
-    divCardBody.appendChild(purchaseLink);
+      let move = createNewElement('p', 'card-text');
+      move.textContent = `Move: ${newPokemon.move}`;
 
-    divCard.appendChild(img);
-    divCard.appendChild(divCardBody);
+      let purchaseLink = createNewElement('a', 'btn');
+      purchaseLink.classList.add('btn-primary');
+      purchaseLink.href = newPokemon.purchaseLink;
+      purchaseLink.textContent = 'Purchase';
 
-    container.appendChild(divCard);
+      // Add new HTML elements to the DOM
+      divCardBody.appendChild(name);
+      divCardBody.appendChild(height);
+      divCardBody.appendChild(weight);
+      divCardBody.appendChild(ability);
+      divCardBody.appendChild(move);
+      divCardBody.appendChild(purchaseLink);
 
-  })
-  .catch(error => {
-    console.log(error);
-  });
+      divCard.appendChild(img);
+      divCard.appendChild(divCardBody);
+
+      container.appendChild(divCard);
+
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
 createNewCard('charizard');
